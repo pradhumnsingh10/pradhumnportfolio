dea1616f4923ee0295a2d15e8b302acdfaaaf323
pradhumnsingh45 <60345106+pradhumnsingh45@users.noreply.github.com>
2020-12-23 23:36:30 +0530

    Updates

---
+++ precache-manifest.3cf68d92b020e1cf939d0060062a8cda.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac594dd84e0d54ebc9d8dc2fc1795a0",
+    "url": "/pradhumnsingh10/deploy.git/index.html"
+  },
+  {
+    "revision": "8bbb0f48b12cb5b43a85",
+    "url": "/pradhumnsingh10/deploy.git/static/css/main.ac51fde7.chunk.css"
+  },
+  {
+    "revision": "3ccfe02af966a410ec63",
+    "url": "/pradhumnsingh10/deploy.git/static/js/2.18c4c999.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/pradhumnsingh10/deploy.git/static/js/2.18c4c999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bbb0f48b12cb5b43a85",
+    "url": "/pradhumnsingh10/deploy.git/static/js/main.83decd56.chunk.js"
+  },
+  {
+    "revision": "a9f6abf1eab013cc97f4",
+    "url": "/pradhumnsingh10/deploy.git/static/js/runtime-main.ee5aeade.js"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/pradhumnsingh10/deploy.git/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/pradhumnsingh10/deploy.git/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "97ee8a84df831f8e5028051022267c62",
+    "url": "/pradhumnsingh10/deploy.git/static/media/profile.97ee8a84.webp"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/pradhumnsingh10/deploy.git/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/pradhumnsingh10/deploy.git/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/pradhumnsingh10/deploy.git/static/media/slide3.8e5b87b6.webp"
+  }
+]);
